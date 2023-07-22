9e08c57d62d6b2ff76302abf5bd4108e32f0e33f
LAPTOP-FJB0KG28\augus <augustus.marquis@gmail.com>
2023-07-22 09:38:27 -0400

    change key

--- grand-line-ui/utils/fetchData.js
+++ grand-line-ui/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'af2afe6764mshdf36744e5058163p1bcb75jsn92a988eb2841',
+    'X-RapidAPI-Key': 'KEY_HERE',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   },
 };
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'X-RapidAPI-Key': 'KEY_HERE',
   },
 };
 
